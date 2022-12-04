2d6ea6013bafaac65af38f22a50c9ce2d1dd99e7
roshan71 <patelroshan882@gmail.com>
2022-12-04 22:17:23 +0530

    hh

--- pages/User/index.js
+++ pages/User/index.js
@@ -6,7 +6,7 @@ import { app } from "../../utils/firebase";
 import {  doc, setDoc,getDoc } from 'firebase/firestore';
 import { async } from "@firebase/util";
 import { useEffect } from "react";
-const {servus}=require('../../secert.json')
+
 
 const User=()=>{
 
